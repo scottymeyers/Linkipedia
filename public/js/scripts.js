e3cb76e6b8653332316d44f5b9a5c129da39ac20
scott meyers <scottmey@gmail.com>
2015-12-06 18:41:32 -0500

    adjust number and date filtering

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,9 @@
 $(function(){
 
   // table filtering on history
-  new Tablesort(document.getElementById('history'), {});
+  if ( $('#history').length ) {
+    new Tablesort(document.getElementById('history'), {});
+  }
 
   // foundation (forms)
 	$(document).foundation();
@@ -78,3 +80,55 @@ function visualize(json){
 
   });
 }
+
+// tablesort.number.js
+(function(){
+  var cleanNumber = function(i) {
+    return i.replace(/[^\-?0-9.]/g, '');
+  },
+
+  compareNumber = function(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+
+    a = isNaN(a) ? 0 : a;
+    b = isNaN(b) ? 0 : b;
+
+    return a - b;
+  };
+
+  Tablesort.extend('number', function(item) {
+    return item.match(/^-?[£\x24Û¢´€]?\d+\s*([,\.]\d{0,2})/) || // Prefixed currency
+      item.match(/^-?\d+\s*([,\.]\d{0,2})?[£\x24Û¢´€]/) || // Suffixed currency
+      item.match(/^-?(\d)*-?([,\.]){0,1}-?(\d)+([E,e][\-+][\d]+)?%?$/); // Number
+  }, function(a, b) {
+    a = cleanNumber(a);
+    b = cleanNumber(b);
+
+    return compareNumber(b, a);
+  });
+}());
+
+// Basic dates in dd/mm/yy or dd-mm-yy format.
+// Years can be 4 digits. Days and Months can be 1 or 2 digits.
+(function(){
+  var parseDate = function(date) {
+    date = date.replace(/\-/g, '/');
+    date = date.replace(/(\d{1,2})[\/\-](\d{1,2})[\/\-](\d{2})/, '$1/$2/$3'); // format before getTime
+
+    return new Date(date).getTime() || -1;
+  };
+
+  Tablesort.extend('date', function(item) {
+    return (
+      item.search(/(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\.?\,?\s*/i) !== -1 ||
+      item.search(/\d{1,2}[\/\-]\d{1,2}[\/\-]\d{2,4}/) !== -1 ||
+      item.search(/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/i) !== -1
+    ) && !isNaN(parseDate(item));
+  }, function(a, b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+
+    return parseDate(b) - parseDate(a);
+  });
+}());
