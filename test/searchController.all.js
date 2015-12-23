bb47ee2fe943726e2e08caaca79ac4a64a78fddc
scott meyers <scottmey@gmail.com>
2015-12-22 23:03:28 -0500

    continue testing routes

--- test/searchController.all.js
+++ test/searchController.all.js
@@ -1,18 +1,18 @@
 var app    = require('../server');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-// var expect  = require('chai').expect;
 var should = chai.should();
+// var expect  = require('chai').expect;
+
 
 chai.use(chaiHttp);
 
 
 describe('Searches', function() {
 
-  it('should display ALL searches on /history GET', function(done) {
+  it('should display history in HTML on /history GET', function(done) {
     chai.request(app)
       .get('/history')
-      //.auth('dangler', 'dangler')
       .end(function(err, res){
         res.should.have.status(200);
         res.should.be.html;
@@ -20,4 +20,30 @@ describe('Searches', function() {
       });
   });
 
+  it('should display ALL searches in JSON on /api/searches GET', function(done) {
+    chai.request(app)
+      .get('/api/searches')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('_id');
+        res.body[0].should.have.property('body');
+        res.body[0].should.have.property('depth');
+        res.body[0].should.have.property('pages_searched');
+        res.body[0].should.have.property('pending');
+        res.body[0].should.have.property('urls');
+        done();
+      });
+  });
+
 });
+
+
+// NOTES
+/*
+
+.get('/')
+  .auth('', '')
+
+*/
