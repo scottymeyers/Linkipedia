d6a7fc848a1dd58e423a93433df17193d983f50f
scott meyers <scottmey@gmail.com>
2015-12-23 13:39:19 -0500

    include test for single search GET request

---
+++ app/config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.mongoURI = {
+  development: 'mongodb://localhost/wiki',
+  test: 'mongodb://localhost/node-test'
+}
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 // set up ===============================================
 var auth       = require('http-auth');
 var bodyParser = require('body-parser');
-var database   = require('./config/database');
+var config     = require('./app/config');
 var mongoose   = require('mongoose');
 var path       = require('path');
 var sass       = require('node-sass-middleware')
 var express    = require('express');
 var app        = express();
 
+// mongoose =============================================
+mongoose.connect(config.mongoURI[app.settings.env], function(err, res) {
+  if(err)
+    console.log('Error connecting to the database. ' + err);
+});
+
 // configuration ========================================
 var port = process.env.PORT || 8081;
-var db   = process.env.MONGOLAB_URI || database.url;
 
 app.locals.moment = require('moment');
-mongoose.connect(db);
+
+// globals ==============================================
 global.appRoot = path.resolve(__dirname);
 
-// protect on production for now
+// protect on production ================================
 if ('production' == app.get('env')) {
   var basic = auth.basic({
       realm: "Private",
