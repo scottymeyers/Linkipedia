18d281981be09079f948da72a878b14fe7ef3adc
Scott Meyers <scott@siberia.io>
2020-12-18 18:55:42 -0500

    strips out obsolete bits

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,6 @@ module.exports = (app) => {
 		res.header('Access-Control-Allow-Credentials', 'true');
 		next();
 	});
-
 	app.get('/', (req, res) => res.render('index', { path: req.path }));
 	app.post('/scrape', (req, res) => searchController.createSearch(req, res));
 	app.get('*', (req, res) => res.redirect('/'));
