07a5302695f4cf5d07dc2aaf1b19590d4b70abb3
Scott Meyers <scott@siberia.io>
2021-02-09 00:17:25 -0500

    consolidates queue

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const express = require('express');
+const cheerio = require('cheerio');
+const { default: PQueue } = require('p-queue');
+const fetch = require('node-fetch')
 
 const app = express();
 const server = require('http').createServer(app);
@@ -8,17 +11,85 @@ const io = require('socket.io')(server);
 
 app.locals.moment = require('moment');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+/** routes */
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+app.get('/', (req, res) => res.render('index', { path: req.path }));
+app.post('/crawl', (req, res) => {
+  const socketio = req.app.get('socketio');
+  const { start, end, exact } = req.body;
+  let id = 1;
+  const links = [];
+  const nodes = [];
+  const queue = new PQueue({ concurrency: 1, timeout: 5000 });
+  
+  const addToQueue = async (url, parentId) => {
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+    const term = exact ? new RegExp('\\b'+ end +'\\b', 'gi') : new RegExp(end, 'gi');
+    const found = $('#bodyContent').text().match(term);
+    const urlId = id++;
+    const u = {
+      id: url,
+      nid: urlId,
+      parentId,
+    };
+    nodes.push(u);
+    links.push({
+      source: urlId,
+      target: parentId,
+    })
+    console.log(`-> ${url}`)
+    if (found) {
+      queue.clear();
+      socketio.emit('results', {
+        /** TODO: return only the sequence not all the nodes */
+        message: `Found in: ${url}`,
+        results: {
+          links,
+          nodes,
+        },
+      });
+    } else {
+      socketio.emit('message', {
+        message: `Searching: ${url}`,
+      });
+      $('#bodyContent a').each((_, value) => {
+        const href = $(value).attr('href');
+        if (href && href.startsWith('/wiki/') && !href.includes('(identifier)')) {
+          queue.add(() => addToQueue(`https://en.wikipedia.org${href}`, urlId));
+        }
+      });
+    }
+  };
+  
+  // initialize
+  queue.add(() => addToQueue(`https://en.wikipedia.org/wiki/${start}`, 0));
+  
+  res.status(200).json({
+    data: {
+      message: 'initializing search'
+    }
+  });
+});
+app.get('*', (_, res) => res.redirect('/'));
+
+/** sockets */
 app.set('socketio', io);
 
 io.sockets.on('connection', (socket) => {
   socket.emit('message', { message: 'Connected' });
-  // socket.on('message', (message) => console.log(message));
 });
-
-require('./app/routes')(app, io);
 
 server.listen(process.env.PORT || 3000);
--- public/scripts.js
+++ public/scripts.js
@@ -35,20 +35,52 @@ const setMessage = (message) => {
 
 const setResults = (results) => {
   table.parentNode.style.display = 'block';
-  const { urls } = results;
-  urls.map((url) => {
-    if (!url) return null;
-    const newRow = table.insertRow();
-    newRow.insertCell(0).innerHTML = url.id;
-    newRow.insertCell(1).innerHTML = url.parentId !== 0 ? url.parentId : '';
-    newRow.insertCell(2).innerHTML = `<a href='${url.href}' target="_blank">${url.href.replace('https://en.wikipedia.org', '')}</a>`;
-    return newRow;
-  });
+  const { nodes, links } = results;
+  console.log(results);
+
+  /** Graph */
+  // eslint-disable-next-line no-undef
+  const Graph = ForceGraph3D()(document.getElementById('graph'));
+  Graph.resetProps();
+  Graph.graphData({
+    nodes,
+    links: [],
+  })
+    .nodeAutoColorBy('group')
+    // .backgroundColor('rgba(37, 138, 216, 0.07)')
+    .backgroundColor('orange')
+    .height('1000')
+    .width('1000')
+    .nodeColor('red')
+    .linkColor('orange')
+    .nodeAutoColorBy('parentId')
+    .nodeThreeObject((node) => {
+      // eslint-disable-next-line no-undef
+      const sprite = new SpriteText(node.id);
+      sprite.material.depthWrite = false; // make sprite background transparent
+      sprite.color = 'black';
+    
+      sprite.textHeight = 8;
+      return sprite;
+    });
+
+  // Spread nodes a little wider
+  Graph.d3Force('charge').strength(-120);
+
+  /** TODO: Table */
+  // urls.map((url) => {
+  //   if (!url) return null;
+  //   const newRow = table.insertRow();
+  //   newRow.insertCell(0).innerHTML = url.id;
+  //   newRow.insertCell(1).innerHTML = url.parentId !== 0 ? url.parentId : '';
+  //   newRow.insertCell(2).innerHTML = `<a href='${url.href}' target="_blank">${url.href.replace('https://en.wikipedia.org', '')}</a>`;
+  //   return newRow;
+  // });
 };
 
 const getInputValue = (name) => document.querySelector(`[name="${name}"]`).value;
 
-searchForm.addEventListener('submit', (event) => {
+searchForm.addEventListener('submit',  async (event) => {
   event.preventDefault();
   const formData = {
     start: getInputValue('start'),
@@ -58,13 +90,18 @@ searchForm.addEventListener('submit', (event) => {
   setLoadingStyles(true);
   setError(null);
   setMessage(null);
+
   fetch('/crawl', {
     body: JSON.stringify(formData),
     headers: {
       'Content-Type': 'application/json',
     },
     method: 'POST',
-  }).catch((e) => console.log(`error: ${e}`));
+  }).then((response) => {
+    return response.json();
+  }).then((json) => {
+    setMessage(json.data.message);
+  });
 });
 
 (() => {
